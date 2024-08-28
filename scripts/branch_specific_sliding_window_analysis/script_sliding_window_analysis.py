cc41d0d7a3196fb3e577e24af7d248308850cf66
Christiane Elgert <christiane.elgert@gmx.de>
2024-08-28 11:19:20 +0200

    small change

--- scripts/branch_specific_sliding_window_analysis/script_sliding_window_analysis.py
+++ scripts/branch_specific_sliding_window_analysis/script_sliding_window_analysis.py
@@ -7,66 +7,11 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
-from utils.script_handle_data import (
-    process_gene_directory_components
+from utils.script_handle_satute_components import (
+    calculate_variance, 
+    summarize_component_data,
 )
 
-
-def summarize_component_data(results_dir, output_dir, data_name=None):
-    if data_name is None: 
-        data_name = os.path.basename(results_dir)
-
-    summarized_component_data = {}
-
-    print("Summarize coherence coefficient data:")
-    
-    # List all subdirectories in the results_dir
-    subdirs = [d for d in os.listdir(results_dir) if os.path.isdir(os.path.join(results_dir, d))]
-
-    if subdirs:  # If there are subdirectories, process each one
-        for dataset_name in subdirs:
-            print(f"Processing subfolder: {dataset_name}")
-            gene_directory = os.path.join(results_dir, dataset_name)
-            
-            # Analysis summary for specific gene directory
-            gene_component_data = process_gene_directory_components(gene_directory, dataset_name)
-            
-            if not gene_component_data.empty:
-                # Sort the data by the "site" column and append to the summarized data
-                gene_component_data.sort_values(by=["branch", "site"], inplace=True)
-                summarized_component_data[dataset_name] = gene_component_data
-
-    
-    # If there are no subdirectories, process the results_dir itself
-    print(f"Processing: {results_dir}")
-    gene_component_data = process_gene_directory_components(results_dir, os.path.basename(results_dir))
-    
-    if not gene_component_data.empty:
-        gene_component_data.sort_values(by=["branch", "site"], inplace=True)
-        summarized_component_data[os.path.basename(results_dir)] = gene_component_data
-
-    if summarized_component_data:  # Checking if the dictionary is not empty
-        # Aggregate all DataFrames into a single DataFrame
-        all_components_data = pd.concat(summarized_component_data.values(), ignore_index=True)
-        
-        # Save the aggregated data
-        csv_file_path = os.path.join(output_dir, f"{data_name}_summarized_component_data.csv")
-        all_components_data.to_csv(csv_file_path, index=False)
-
-    print("")
-    return summarized_component_data
-
-
-def calculate_variance(component_dataframe):
-    variance = 0
-    rate_counts = component_dataframe['rate_category'].value_counts()
-    sequence_len = sum(rate_counts)
-    variance_by_rate = component_dataframe.groupby('rate_category')['category_variance'].first()
-    for rate, count in rate_counts.items():
-        variance += variance_by_rate[rate] * count/sequence_len
-    return variance
-
-        
 def calculate_window_zscore(window_data,variance):
     window_size = len(window_data)
     weights = np.linspace(1, 0.5, window_size)  # Linearly decreasing weights
@@ -129,7 +74,7 @@ def sliding_window_analysis(satute_input_dir, window_size, edge_list=None, resul
 
             # Save the sliding window results for the current dataset
             sliding_window_csv_path = os.path.join(results_dir, f"{dataset}_sliding_window_size_{window_size}.csv")
-            sliding_window_df.to_csv(sliding_window_csv_path, index=False)
+            sliding_window_dict.to_csv(sliding_window_csv_path, index=False)
 
     if global_variance_list:  # Check if global_variance_list is not empty
         # Convert the list of dictionaries into a DataFrame

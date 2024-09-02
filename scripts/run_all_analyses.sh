9437caa8d685adbe1b92e642bd322db2c3d0f9df
Christiane Elgert <christiane.elgert@gmx.de>
2024-09-02 09:54:20 +0200

    all example analyses

---
+++ scripts/run_all_analyses.sh
@@ -0,0 +1,26 @@
+python=python3
+
+rm -r ../example/results*
+
+# branch specific sliding analysis
+cd branch_specific_sliding_window_analysis/
+$python  script_sliding_window_analysis.py
+$python  script_visualise_results.py 
+
+# per category analyses
+cd ../per_category_analysis/
+$python script_category_analysis.py 
+
+# per region analysis
+cd ../per_alignment_region_analysis
+$python script_per_region_analysis.py
+
+#missing separate analysis
+
+# z-score differences between  branches
+cd  ../zscore_differences_between_branches
+$python script_per_region_zscore_differences_between_branches.py
+
+# z-score differnces between topologies
+cd  ../zscore_differences_between_topologies
+$python script_per_region_zscore_differences_between_topologies.py
